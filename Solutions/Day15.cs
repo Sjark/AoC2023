f675dc7092079e51e255b6da4e4958edee82fb08
Lasse Meland <sjark89@gmail.com>
2023-12-16 13:43:42 +0100

    Day 15b

--- Solutions/Day15.cs
+++ Solutions/Day15.cs
@@ -1,3 +1,6 @@
+using System.Collections.Specialized;
+using System.Reflection.Emit;
+
 namespace AoC2023;
 
 public class Day15 : ISolution
@@ -23,5 +26,58 @@ public class Day15 : ISolution
         }
 
         Console.WriteLine($"Day15a: {total}");
+
+        var boxes = Enumerable.Range(0, 256)
+            .Select(a => new OrderedDictionary())
+            .ToList();
+
+        foreach (var instruction in input.Split(','))
+        {
+            var hash = 0;
+
+            var label = instruction.Split(['=', '-']).First();
+
+            foreach (var character in label)
+            {
+                hash += character;
+                hash *= 17;
+                hash %= 256;
+            }
+
+            if (instruction.Contains('='))
+            {
+                var focalLength = int.Parse(instruction.Split('=').Last());
+
+                if (boxes[hash].Contains(label))
+                {
+                    boxes[hash][label] = focalLength;
+                }
+                else
+                {
+                    boxes[hash].Add(label, focalLength);
+                }
+            }
+            else
+            {
+                if (boxes[hash].Contains(label))
+                {
+                    boxes[hash].Remove(label);
+                }
+            }
+        }
+
+        total = 0;
+
+        for (var boxI = 0; boxI < boxes.Count; boxI++)
+        {
+            var box = boxes[boxI];
+
+            for (var i = 0; i < box.Count; i++)
+            {
+                total += (boxI + 1) * (i + 1) * (int)box[i]!;
+            }
+        }
+
+        Console.WriteLine($"Day15b: {total}");
     }
 }
---
+++ Solutions/Day16.cs
@@ -0,0 +1,9 @@
+namespace AoC2023;
+
+public class Day16 : ISolution
+{
+    public void Execute()
+    {
+        var input = File.ReadAllLines("Solutions/Day16Input.txt");
+    }
+}
