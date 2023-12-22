a579fc4f926c939db5114d0fe09cc06833341947
Lasse Meland <mlass@equinor.com>
2023-12-22 08:16:30 +0100

    Day 21a

--- Solutions/Day21.cs
+++ Solutions/Day21.cs
@@ -1,3 +1,5 @@
+using Microsoft.VisualBasic;
+
 namespace AoC2023;
 
 public class Day21 : ISolution
@@ -26,48 +28,42 @@ public class Day21 : ISolution
             }
         }
 
-        Move(grid, startPos, 64);
+        var queue = new Queue<(Coord Coord, int StepsLeft)>();
+        queue.Enqueue((startPos, 64));
+
+        while (queue.TryDequeue(out var currentTile))
+        {
+            if (currentTile.StepsLeft == 0)
+            {
+                _lastTiles.Add(currentTile.Coord);
+                continue;
+            }
+            else if (_visitedTiles.Contains(currentTile.Coord) && currentTile.StepsLeft % 2 == 0)
+            {
+                _lastTiles.Add(currentTile.Coord);
+                continue;
+            }
+
+            if (currentTile.Coord.Y - 1 >= 0 && grid[currentTile.Coord.Y - 1, currentTile.Coord.X] != '#')
+            {
+                queue.Enqueue((currentTile.Coord with { Y = currentTile.Coord.Y - 1 }, currentTile.StepsLeft - 1));
+            }
+            if (currentTile.Coord.Y + 1 < grid.GetLength(0) && grid[currentTile.Coord.Y + 1, currentTile.Coord.X] != '#')
+            {
+                queue.Enqueue((currentTile.Coord with { Y = currentTile.Coord.Y + 1 }, currentTile.StepsLeft - 1));
+            }
+            if (currentTile.Coord.X - 1 >= 0 && grid[currentTile.Coord.Y, currentTile.Coord.X - 1] != '#')
+            {
+                queue.Enqueue((currentTile.Coord with { X = currentTile.Coord.X - 1 }, currentTile.StepsLeft - 1));
+            }
+            if (currentTile.Coord.X + 1 < grid.GetLength(1) && grid[currentTile.Coord.Y, currentTile.Coord.X + 1] != '#')
+            {
+                queue.Enqueue((currentTile.Coord with { X = currentTile.Coord.X + 1 }, currentTile.StepsLeft - 1));
+            }
+
+            _visitedTiles.Add(currentTile.Coord);
+        }
 
         Console.WriteLine($"Day 21a: {_lastTiles.Count}");
-    }
-
-    private void Move(char[,] grid, Coord location, int stepsLeft)
-    {
-
-        if (stepsLeft == 0)
-        {
-            _lastTiles.Add(location);
-            return;
-        }
-        else if (_visitedTiles.Contains(location))
-        {
-            return;
-        }
-
-        _visitedTiles.Add(location);
-
-        if (stepsLeft % 2 == 0)
-        {
-            _lastTiles.Add(location);
-        }
-
-        stepsLeft--;
-
-        if (location.Y - 1 >= 0 && grid[location.Y - 1, location.X] != '#')
-        {
-            Move(grid, location with { Y = location.Y - 1 }, stepsLeft);
-        }
-        if (location.Y + 1 < grid.GetLength(0) && grid[location.Y + 1, location.X] != '#')
-        {
-            Move(grid, location with { Y = location.Y + 1 }, stepsLeft);
-        }
-        if (location.X - 1 >= 0 && grid[location.Y, location.X - 1] != '#')
-        {
-            Move(grid, location with { X = location.X - 1 }, stepsLeft);
-        }
-        if (location.X + 1 < grid.GetLength(1) && grid[location.Y, location.X + 1] != '#')
-        {
-            Move(grid, location with { X = location.X + 1 }, stepsLeft);
-        }
     }
 }
